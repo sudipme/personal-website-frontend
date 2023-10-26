e96dc8c211872d7a0669859a2b45d5ae224ada8f
Sudip Halder <prance00_wheel@icloud.com>
2023-10-26 18:50:12 +0530

    first api test with sample contactme page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css';
 import HomePage from './HomePage';
+import ContactPage from './ContactPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path='/' element={<HomePage />} >
-          <Route exact path='/me' element={<HomePage />} />
-        </Route>
+        <Route exact path='/' element={<HomePage />} />
+        <Route exact path='contact' element={<ContactPage />} />
+       
       </Routes>
     </BrowserRouter>
   );
---
+++ src/ContactPage.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+
+export default function ContactPage(){
+    const [message, setMessage] = useState(''); // State to store the message
+
+  useEffect(() => {
+    // Send the API request using Fetch
+    fetch('http://localhost:8000/api/data')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        // Assuming the API response is in the form { "message": "your_value_here" }
+        const messageValue = data.message;
+        setMessage(messageValue); // Update the state with the message value
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []); // Empty dependency array ensures this effect runs once on component mount
+
+    return(
+        <h1>Contact page message:{message}</h1>
+    );
+}
