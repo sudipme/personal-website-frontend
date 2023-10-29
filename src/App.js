57ab19076f20f7ad0ae79097d8cdbd1c91b5224f
Sudip Halder <prance00_wheel@icloud.com>
2023-10-29 23:13:23 +0530

    removed contact page file and added api connection test file

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/HomePage';
-import ContactPage from './pages/ContactPage';
-import GithubHeatMap from './components/GithubActivity';
-
+import ApiConnectionTest from './pages/ApiConnectionTest';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path='/' element={<HomePage />} />
-        <Route exact path='contact' element={<ContactPage />} />
-        <Route exact path='git' element={<GithubHeatMap/>} />
+        <Route exact path='/backend' element={<ApiConnectionTest />} />
       </Routes>
     </BrowserRouter>
   );
