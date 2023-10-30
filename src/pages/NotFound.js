9102606b8c3addaa6d7576c1520d603397c09aa6
Sudip Halder <prance00_wheel@icloud.com>
2023-10-31 01:10:58 +0530

    little position changes

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,12 +1,11 @@
 import React from "react";
-import NavLink from "../components/HomePage/NavLinks";
 import '../css/NotFound.css'
 import pageNotFoundImg from '../images/page-not-found-img.png';
 
 function NotFound(){
     return(
         <div id="main-container">
-            <img id="not-found-img" src={pageNotFoundImg}></img>
+            <img id="not-found-img" src={pageNotFoundImg} alt="404 not found illustration"></img>
             <h1 id="not-found-headline">Oops! Page Not in This Galaxy</h1>
             <p id="not-found-para" >Please check the link again.</p>
             <a id="home-link" href="https://sudip.me"> ⬅ Back to home</a>
