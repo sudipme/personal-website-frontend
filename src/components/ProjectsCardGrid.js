ad08f0e18cf228c039d22c72a464e0970ec12f58
Sudip Halder <87203402+sudipme@users.noreply.github.com>
2024-05-19 23:32:18 +0530

    ui-v3.0 homepage modified ui

--- src/components/ProjectsCardGrid.js
+++ src/components/ProjectsCardGrid.js
@@ -1,82 +1,87 @@
-import React from 'react';
-import '../css/ProjectsCardGrid.css';
-import { BaseUrl, ApiBaseUrl } from '../config.js';
+import React from "react";
+import "../css/ProjectsCardGrid.css";
+import { BaseUrl, ApiBaseUrl } from "../config.js";
 
-function ProjectCard(props){
-    const containerStyle = {
-        height: '175px',
-        borderRadius: '20px',
-        border: '0.5px solid rgba(0, 0, 0, 0.1)',
-        background: '#FFFFFF',
-        filter: 'drop-shadow(0px 1px 1px rgba(0, 0, 0, 0.20))',
-        cursor: 'pointer',
-        overflow: 'hidden',
-    }
-    const titleStyle = {
-        textAlign: 'left',
-        marginLeft: "30px",
-        color: '#000',
-        fontFamily: 'Exo, sans-serif',
-        fontSize: '20px',
-        fontStyle: 'normal',
-        fontWeight: 500,
-        lineHeight: 'normal'
-    }
-    const descriptionStyle = {
-        marginLeft: "30px",
-        textAlign: 'left',
-        color: '#000',
-        fontFamily: 'Exo, sans-serif',
-        fontSize: '16px',
-        fontStyle: 'normal',
-        fontWeight: 400,
-        lineHeight: 'normal'
-    }
-   
-    return(
-        <div onClick={() => window.open(props.link, '_blank')} className='project-card' style={containerStyle} >
-            <h3 id="project-title" style={titleStyle}>{props.title}</h3>
-            <p id="project-desctiption" style={descriptionStyle}>{props.description}</p>
-        </div>
-    )
+function ProjectCard(props) {
+  const containerStyle = {
+    height: "175px",
+    borderRadius: "20px",
+    border: "0.5px solid rgba(0, 0, 0, 0.1)",
+    background: "#222",
+    filter: "drop-shadow(0px 1px 1px rgba(0, 0, 0, 0.20))",
+    cursor: "pointer",
+    overflow: "hidden",
+  };
+  const titleStyle = {
+    textAlign: "left",
+    marginLeft: "30px",
+    color: "#FFF",
+    fontFamily: "Exo, sans-serif",
+    fontSize: "20px",
+    fontStyle: "normal",
+    fontWeight: 500,
+    lineHeight: "normal",
+  };
+  const descriptionStyle = {
+    marginLeft: "30px",
+    textAlign: "left",
+    color: "#ccc",
+    fontFamily: "Exo, sans-serif",
+    fontSize: "16px",
+    fontStyle: "normal",
+    fontWeight: 400,
+    lineHeight: "normal",
+  };
+
+  return (
+    <div
+      onClick={() => window.open(props.link, "_blank")}
+      className="project-card"
+      style={containerStyle}
+    >
+      <h3 id="project-title" style={titleStyle}>
+        {props.title}
+      </h3>
+      <p id="project-desctiption" style={descriptionStyle}>
+        {props.description}
+      </p>
+    </div>
+  );
 }
 
 function ProjectsCardGrid(props) {
+  const { projects } = props;
 
-    const { projects } = props;
+  if (projects === null) {
+    return <div>Loading...</div>;
+  }
 
-    if (projects === null) {
-        return <div>Loading...</div>;
-      }
-    
-    if (!projects || Object.keys(projects).length === 0) {
-        return <div>No data available</div>;
-    }
+  if (!projects || Object.keys(projects).length === 0) {
+    return <div>No data available</div>;
+  }
 
-    const gridStyle = {
-        display: 'grid',
-        rowGap: '20px',
-        columnGap: '20px',
-        overflow: 'hidden',
-    };
-    
-    return (
-        <div id='grid' style={gridStyle}>
-            {
-                Object.keys(props.projects).map((key) => {
-                    const widget = props.projects[key];
-                    return (
-                        <ProjectCard
-                            key={widget.time_stamp}
-                            link={BaseUrl+'blogs/'+widget.project_id}
-                            title={widget.project_title}
-                            description={widget.project_description}
-                        />
-                    );
-                })
-            }
-        </div>
-    )
+  const gridStyle = {
+    display: "grid",
+    rowGap: "20px",
+    columnGap: "20px",
+    overflow: "hidden",
+  };
+
+  return (
+    <div id="grid" style={gridStyle}>
+      {Object.keys(props.projects).map((key) => {
+        const widget = props.projects[key];
+        return (
+          <ProjectCard
+            key={widget.time_stamp}
+            link={BaseUrl + "blogs/" + widget.project_id}
+            title={widget.project_title}
+            description={widget.project_description}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default ProjectsCardGrid;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,132 +1,182 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import { ApiBaseUrl } from "../config.js";
 
-import LoadingAnimation  from '../components/LoadingAnimation.js';
-import TopBar from '../components/TopBar.js';
-import HighlightsRow from '../components/HomePage/Highlights';
-import {FeaturedProjects} from '../components/HomePage/FeaturedProjects';
-import FeaturedBlogs from '../components/HomePage/FeaturedBlogs';
-import Footer from '../components/Footer.js';
-import '../css/HomePage.css';
-import {ApiBaseUrl} from '../config.js';
-
+import LoadingAnimation from "../components/LoadingAnimation.js";
+import TopBar from "../components/TopBar.js";
+import HighlightsRow from "../components/HomePage/Highlights";
+import { FeaturedProjects } from "../components/HomePage/FeaturedProjects";
+import FeaturedBlogs from "../components/HomePage/FeaturedBlogs";
+import Footer from "../components/Footer.js";
+import "../css/HomePage.css";
+import RightArrowIcon from "../assets/icons/right-arrow-icon.svg";
 
 function HomePage() {
-    const [isHighlightsLoaded, setIsHighlightsLoaded] = useState(false);
-    const [isFeaturedProjectsLoaded, setIsFeaturedProjectsLoaded] = useState(false);
-    const [isFeaturedBlogsLoaded, setIsFeaturedBlogsLoaded] = useState(false);
-    const highlightsLoaded = () => {setIsHighlightsLoaded(true);}
-    const featuredProjectsLoaded = () => {setIsFeaturedProjectsLoaded(true);}
-    const featuredBlogsLoaded = () => {setIsFeaturedBlogsLoaded(true);}
+  const [isHighlightsLoaded, setIsHighlightsLoaded] = useState(false);
+  const [isFeaturedProjectsLoaded, setIsFeaturedProjectsLoaded] =
+    useState(false);
+  const [isFeaturedBlogsLoaded, setIsFeaturedBlogsLoaded] = useState(false);
+  const highlightsLoaded = () => {
+    setIsHighlightsLoaded(true);
+  };
+  const featuredProjectsLoaded = () => {
+    setIsFeaturedProjectsLoaded(true);
+  };
+  const featuredBlogsLoaded = () => {
+    setIsFeaturedBlogsLoaded(true);
+  };
 
-    let everythingLoaded = isHighlightsLoaded && isFeaturedProjectsLoaded && isFeaturedBlogsLoaded;
+  let everythingLoaded =
+    isHighlightsLoaded && isFeaturedProjectsLoaded && isFeaturedBlogsLoaded;
 
-    const homePageStyle = {
-        width: "100vw",
-        minHeight:"100vh",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "flex-around",
-        alignItems: "center",
-        overflow: "hidden",
-    }
+  const homePageStyle = {
+    width: "100vw",
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-around",
+    alignItems: "center",
+    overflow: "hidden",
+  };
 
-    const loadingAnimationContainerStyle = {
-        width:"100vw",
-        height:"100vh",
-        display:"flex",
-        justifyContent: "center",
-        alignItems: "center",
-    }
+  const loadingAnimationContainerStyle = {
+    width: "100vw",
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
 
-    return (
+  return (
     <>
-        <div style={everythingLoaded ?{ display: "none" } :loadingAnimationContainerStyle }>
-            <LoadingAnimation/>
-        </div>
+      <div
+        style={
+          everythingLoaded
+            ? { display: "none" }
+            : loadingAnimationContainerStyle
+        }
+      >
+        <LoadingAnimation />
+      </div>
 
-        <div id='home-page'  style={everythingLoaded ? homePageStyle : { display: "none" }}>
-            <TopBar />
-            <Spacer height="20px" />
+      <div
+        id="home-page"
+        style={everythingLoaded ? homePageStyle : { display: "none" }}
+      >
+        <TopBar />
+        <Spacer height="20px" />
 
-            <div ></div>
-            <AboutMe />
-            <Spacer height="20px" />
+        <div></div>
+        <AboutMe />
+        <Spacer height="20px" />
 
-            <Heading content="Highlights" />
-            <HighlightsRow componentLoaded={highlightsLoaded}/>
+        <HighlightsRow componentLoaded={highlightsLoaded} />
 
-            <Spacer height="20px" />
+        <Spacer height="20px" />
 
-            <Heading content="Projects" />
-            <FeaturedProjects componentLoaded={featuredProjectsLoaded} />
-            <Spacer height="20px" />
-            <FeaturedBlogs conponentLoaded={featuredBlogsLoaded} />
-            <Footer />
-        </div>
-        </>
-    )
+        <Heading title="Projects" link="/projects" />
+        <FeaturedProjects componentLoaded={featuredProjectsLoaded} />
+        <Spacer height="20px" />
+        <FeaturedBlogs conponentLoaded={featuredBlogsLoaded} />
+        <Footer />
+      </div>
+    </>
+  );
 }
 
 function AboutMe() {
-    const [aboutMeText, setAboutMeText] = useState("");
-    const paragraphStyle = {
-        color: "#000",
-        fontFamily: "Raleway",
-        fontStyle: "normal",
-        fontWeight: "500",
-        lineHeight: "normal",
+  const [aboutMeText, setAboutMeText] = useState("");
+  const paragraphStyle = {
+    color: "#000",
+    fontFamily: "Raleway",
+    fontStyle: "normal",
+    fontWeight: "500",
+    lineHeight: "normal",
+  };
+  const firstLineStyle = {
+    display: "block",
+    lineHeight: "40px",
+  };
+
+  useEffect(() => {
+    fetch(ApiBaseUrl + "about-me-text")
+      .then((response) => response.json())
+      .then((data) => {
+        setAboutMeText(data.about_me);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (aboutMeText !== "") {
+      document.getElementById("about-me").innerHTML = aboutMeText.replace(
+        /\n/g,
+        "<br>",
+      );
     }
-    const firstLineStyle = {
-        display: "block",
-        lineHeight: "40px",
-    }
-    
-    useEffect(() => {
-        fetch(ApiBaseUrl + 'about-me-text')
-        .then(response => response.json())
-        .then(data => {
-            setAboutMeText(data.about_me);
-        })
-    }, [])
+  });
 
-    useEffect(() => {
-        if(aboutMeText !== ""){
-            document.getElementById('about-me').innerHTML = aboutMeText.replace(/\n/g, "<br>");
-        }
-    })
-
-    return (
-        <div id='about-me-container'>
-
-            <p id='about-me' style={paragraphStyle}>
-                <span style={firstLineStyle}>Hi, I am Sudip.</span>
-            </p>
-        </div>
-    )
+  return (
+    <div id="about-me-container">
+      <p id="about-me" style={paragraphStyle}>
+        <span style={firstLineStyle}>Hi, I am Sudip.</span>
+      </p>
+    </div>
+  );
 }
 
-function Heading(props){
-    const headingStyle = {
-        color: "#888", //#004061
-        fontFamily: "Raleway",
-        fontStyle: "normal",
-        fontWeight: "600",
-        lineHeight: "normal",
-    }
-    return (
-        <div id="heading-container">
-            <h1 id="heading" style={headingStyle}>{props.content}</h1>
-        </div>
-    )
+function Heading(props) {
+  const headingContainerStyle = {
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    cursor: "pointer",
+  };
+  const headingStyle = {
+    fontSize: "24px",
+    color: "#888",
+    fontFamily: "Raleway",
+    fontStyle: "normal",
+    fontWeight: "600",
+    lineHeight: "normal",
+  };
+  const rightArrowIconStyle = {
+    fontSize: "28px",
+    fontWeight: "400",
+    width: "20px",
+  };
+  return (
+    <div
+      style={headingContainerStyle}
+      onClick={() => window.open(props.link, "_blank")}
+    >
+      <h1 style={headingStyle}>{props.title} &nbsp;</h1>
+      <img
+        src={RightArrowIcon}
+        style={rightArrowIconStyle}
+        alt="expand button"
+      ></img>
+    </div>
+  );
 }
 
-function Spacer(props){
-    const spacerStyle = {
-        height: props.height,
-    }
-    return(
-        <div id="spacer" style={spacerStyle}></div>
-    )
+function Spacer(props) {
+  const spacerStyle = {
+    height: props.height,
+  };
+  return <div id="spacer" style={spacerStyle}></div>;
 }
+
+// function BackgroundBlock() {
+//   const blockStyle = {
+//     width: "100vw",
+//     height: "100vh",
+//     backgroundColor: "#222",
+//     position: "absolute",
+//     top: "50%",
+//     left: "0",
+//     zIndex: "-10",
+//   };
+//   return <div id="background-block" style={blockStyle}></div>;
+// }
 
 export default HomePage;
